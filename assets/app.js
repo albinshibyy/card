f82e3670aa49553aa2971c83d74a25e0dc14876b
Albin Shiby <178146421+albinshibyy@users.noreply.github.com>
2025-02-03 14:28:09 +0530

    Add files via upload

--- assets/app.js
+++ assets/app.js
@@ -1,17 +1,17 @@
-const image = document.querySelector(".image");
-const hover = document.querySelector(".hover");
-const modal = document.querySelector(".modal");
-const close = document.querySelector(".close");
+const image = document.querySelector('.image');
+const hover = document.querySelector('.hover');
+const modal = document.querySelector('.modal');
+const close = document.querySelector('.close');
 
-function show() {
-  hover.classList.add("active");
-  modal.classList.add("show");
+function show(){
+    hover.classList.add('active');
+    modal.classList.add('show');
 }
 
-function hide() {
-  hover.classList.remove("active");
-  modal.classList.remove("show");
+function hide(){
+    hover.classList.remove('active');
+    modal.classList.remove('show');
 }
 
-image.addEventListener("click", show);
-close.addEventListener("click", hide);
+image.addEventListener('click', show);
+close.addEventListener('click', hide);
